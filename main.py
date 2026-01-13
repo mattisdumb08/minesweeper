6610156d8d355bdd2f05ed74d644a703e23cfa1e
Matt Huynh <91633363+mattisdumb08@users.noreply.github.com>
2026-01-13 14:33:40 +0000

    Added game loop

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 import pygame as pg
 from map import *
+
+pg.init()
+
+def main():
+
+
+    window = pg.display
+    window.set_mode((600 , 500))
+
+    running = True
+
+    while running:
+
+        segments.draw()
+        bombs.draw()
+
+        window.flip()
+
+        keys = pg.key.get_pressed()
+
+        events = pg.event.get()
+
+        for event in events:
+            if event == pg.QUIT:
+                running = False    
