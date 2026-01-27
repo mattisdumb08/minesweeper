b51eabeed0e408408b63a63f33fca5378b0ee87e
Matt Huynh <20HuynhM@cfbs.islington.sch.uk>
2026-01-27 10:09:25 +0000

    Mouse detection

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def main():
 
     running = True
 
+    leftPressed = False
+    rightPressed = False
+    middlePressed = False
+
     while running:
 
         surface.fill((0 , 0 , 0))
@@ -26,12 +30,28 @@ def main():
         window.flip()
 
         keys = pg.key.get_pressed()
+        mouses = pg.mouse.get_pressed() # Left, Middle , Right
 
         events = pg.event.get()
 
         for event in events:
             if event.type == pg.QUIT:
                 running = False
+        
+        if mouses[0]:
+            location = pg.mouse.get_pos()
+            rect = pg.Rect(pg.rect.RectType(location[0] , location[1]))
+
+            for sprite in map.tiles:
+                if pg.sprite.collide_rect(rect , sprite.rect):
+                    print("Sprite CLicked")
+
+            print("Left Clicked")
+
+        match keys:
+            case pg.K_RIGHT:
+                print("Mouse pressed")
+
 
     return 0   
 
