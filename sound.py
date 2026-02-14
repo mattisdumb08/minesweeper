743e38728c2fb0e6ef05e2eab90d37ea672ac6dd
Matt <20huynhm@centralfoundationboys.co.uk>
2026-02-14 01:57:26 +0000

    Play sound functions (yippee)

--- sound.py
+++ sound.py
@@ -2,7 +2,36 @@ import pygame as pg
 
 pg.mixer.init()
 
-class SoundEffect(pg.mixer.Sound):
+globalVolume = 1
 
-    def __init__(self , fileName):
-        pg.mixer.Sound.__init__(self , )
+pg.mixer.set_num_channels(16)
+
+
+class ChannelWrapper:
+
+    def __init__(self, id):
+        self.channelObject = pg.mixer.Channel(id)
+
+    def playAll(self):
+
+        nextSound = self.channelObject.get_queue()
+
+        while nextSound != None:
+
+            nextSound.play()
+
+            nextSound = self.channelObject.get_queue()
+    
+    def queue(self , newSound : pg.mixer.Sound):
+        self.channelObject.queue(newSound)
+
+soundEffectsChannel = ChannelWrapper(0)
+
+def playYippee():
+
+    yippeeSound = pg.mixer.Sound("Yippee.mp3")
+    yippeeSound.set_volume(globalVolume)
+
+    yippeeSound.play()
+
+    # soundEffectsChannel.queue(yippeeSound)
